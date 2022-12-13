39c8edf0d40a2a0ea0e7b37693fc9acba03e557f
RAguirreGiraldo <raguirregiraldo@gmail.com>
2022-12-13 10:37:12 -0600

    update files in repo

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,56 +1,61 @@
 'use strict';
- 
+
+function select(selector, parent = document) {
+    return parent.querySelector(selector);
+}
+
+const overlay = select('.overlay');
+const loading = select('.loading');
+
 mapboxgl.accessToken = 'pk.eyJ1Ijoicm9iZXJ0b2FndWlycmUxOTc1IiwiYSI6ImNsYmdycXN1MjBpeWQzdnBkbDFka3pscnYifQ.nUFHQUdp9wIInZZGhJfYjQ';
 
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://style/mapbox/streets-v11',
+    /*interactive: false, left boton*/
+    center: [0,0],
+    pitch: 40,
+    zoom: 16
+});
+
+map.dragPan.disable();
+map.keyboard.disable();
+map.scrollZoom.disable();
+map.doubleClickZoom.disable();
+map.touchZoomRotate.disable();
+
+const marker = new mapboxgl.Marker({
+    color: '#3898ff'
+});
+
+function geoLocation(position) {
+    const { longitude, latitude } = position.coords;
+    
+    if (longitude && latitude) {
+        map.setCenter([longitude, latitude]);
+        marker.setLngLat([longitude, latitude]).addTo(map);
+        setTimeout(() => {overlay.style.display = 'none'}, 750);
+    }
+}
+
+function errorHandler(event) {
+    loading.style.animationPlayState = 'paused';
+    console.log(event.message);
+}
+
 const options = {
-    enableHighAccuracy: true
-}
+    enableHighAcurrancy: true,
+    maximumAge:0
+};
 
-let elementSupport = document.getElementById('support');
+/* 
+    The watchPosition() method is used to register a handler finction that will
+    be called automatically each time the position of the device changes
+*/
 
-function error() {
-    elementSupport.innerHTML = "Your location could not be obtained";
-}
- 
-function findMe(posicion) {
-   let elementLatitude = document.getElementById('latitude');
-   let elementLongitude = document.getElementById('longitude');
-
-   let latitude = posicion.coords.latitude;
-   let longitude = posicion.coords.longitude;
- 
-   elementLatitude.innerHTML = "Your Latitude: " + latitude;
-   elementLongitude.innerHTML = "Your Longitude: " + longitude;
- 
-   let map = new mapboxgl.Map({
-       container: 'map',
-       style: 'mapbox://styles/mapbox/streets-v12',
-       center: [longitude, latitude], // starting position
-       zoom: 12
-   });
-      
-   new mapboxgl.Marker().setLngLat([longitude, latitude]).addTo(map); //pointer In map
-}
- 
-// Verifying that my browser supports geolocation.
 
 if (navigator.geolocation) {
-    elementSupport.innerHTML = "Your Browser Supports Geolocation";
-    navigator.geolocation.getCurrentPosition(findMe ,error ,options);
+    navigator.geolocation.getCurrentPosition(geoLocation, errorHandler, options);
 } else {
-    elementSupport.innerHTML = "Your Browser Does Not Support Geolocation";
+    console.log('Geolocation is not supported by your browser');
 }
-
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
